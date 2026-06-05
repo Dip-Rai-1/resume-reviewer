b6d7b796dbf8093cb1ce56d2b9e875a76b4c6fb2
Dip <dipurai815@gmail.com>
2026-06-05 18:42:10 +1000

    feat: update api url to production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export async function analyseResume(resumeText, role) {
-    const response = await axios.post('http://localhost:5000/api/review', {
+    const response = await axios.post('https://resume-reviewer-api-2oua.onrender.com/api/review', {
         resumeText, role
     })
     return response.data;
